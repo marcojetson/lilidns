25c1cda5e87dedeaa4cc7ee4d37c0590f9369aaf
marco <marcojetson@gmail.com>
2015-07-16 20:11:36 +0200

    api update

--- application.rb
+++ application.rb
@@ -1,11 +1,31 @@
+require 'json'
 require 'sinatra'
 require 'sinatra/flash'
+require 'sinatra/subdomain'
 require File.join(Dir.pwd, 'settings')
 require File.join(Dir.pwd, 'assets')
 require File.join(Dir.pwd, 'helpers')
 require File.join(Dir.pwd, 'models')
 
 enable :sessions
+
+subdomain :api do
+  get '/update' do
+    content_type :json
+    begin
+      record_token = RecordToken.first(:token => params['token']) or raise ArgumentError, 'Can not find host'
+      record = record_token.record
+      record.content = params['ip'] || request.ip 
+      record.save
+      @error = ''
+    rescue ArgumentError => e
+      @error = e.message
+    rescue
+      @error = 'Something bad happened'
+    end
+    {:error => @error}.to_json
+  end
+end
 
 get '/' do
   redirect to('/add')
